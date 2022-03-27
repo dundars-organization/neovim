fcfa1396a6934f48be0f30723f6cdd3269c0f7a4
Dundar Göc <gocdundar@gmail.com>
2022-03-27 21:48:28 +0200

    ci: remove reviewers when drafting or closing a PR

---
+++ .github/scripts/remove-reviewers.js
@@ -0,0 +1,10 @@
+module.exports = async ({github, context}) => {
+  const requestedReviewers = await github.rest.pulls.listRequestedReviewers({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number
+  });
+
+  const something = requestedReviewers.data.users.map(e => e.login)
+  console.log(something)
+}
