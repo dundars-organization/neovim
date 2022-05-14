9341f0ccd7db1ae2103b69e38c15abe702d8419f
Dundar Göc <dundar.goc@acumisminds.se>
2022-05-14 15:52:06 +0200

    ci: add rerun command

---
+++ .github/scripts/rerun.js
@@ -0,0 +1,15 @@
+module.exports = async ({ github, context }) => {
+  const pr_data = await github.rest.pulls.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number,
+  });
+
+  const workflow_runs = octokit.rest.actions.listWorkflowRunsForRepo({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  console.log(workflow_runs);
+  //const labels = pr_data.data.labels.map(e => e.name)
+};
