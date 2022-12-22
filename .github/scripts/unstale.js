2706415f9112c5e9b4f3c70fcaab7883103ee269
dundargoc <gocdundar@gmail.com>
2022-12-22 17:28:17 +0100

    fixup: quick update, squash later

--- .github/scripts/unstale.js
+++ .github/scripts/unstale.js
@@ -1,5 +1,10 @@
 module.exports = async ({github, context}) => {
   const commenter = context.actor
-  const issue = await context.issue
+  const issue = octokit.rest.issues.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+  });
+
   console.log(issue)
 }
