956725d04eec2b81f5b1f42f0919446c12999a9e
dundargoc <gocdundar@gmail.com>
2022-12-23 11:14:40 +0100

    fixup: quick update, squash later

--- .github/scripts/unstale.js
+++ .github/scripts/unstale.js
@@ -6,17 +6,18 @@ module.exports = async ({github, context}) => {
     issue_number: context.issue.number,
   });
   const author = issue.data.user.login
+  const labels = issue.data.labels
 
   if(author === commenter){
-    github.rest.issues.removeLabel({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: context.issue.number,
-      name: 'needs:response',
-    });
+    // github.rest.issues.removeLabel({
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo,
+    //   issue_number: context.issue.number,
+    //   name: 'needs:response',
+    // });
   }
 
   // console.log(author)
   // console.log(commenter)
-  console.log(issue)
+  console.log(labels)
 }
